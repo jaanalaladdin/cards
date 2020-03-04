287601f975470f08b173696ed4302a033fd0a6e0
jaanalaladdin <jaanalaladdin@gmail.com>
2020-03-04 17:19:15 +0200

    delete modified

--- delete.php
+++ delete.php
@@ -4,28 +4,11 @@ if($_SESSION['username'] == 'username') {
  include "connect.php";
  print("<html>");
  $conn = connect_db();
- $sql = "SELECT * FROM Addresses";
+ $sql = "DELETE FROM Addresses WHERE id=" . $_GET['id'];
+ // print $sql;
  $result = mysqli_query($conn, $sql);
- if (mysqli_num_rows($result) > 0) {
- // output data of each row
- while($row = mysqli_fetch_assoc($result)) {
- echo $row["id"] . " ";
- echo $row["name"] . " ";
- echo $row["street_address"] . " ";
- echo $row["zip"] . " ";
- echo $row["city"] . " ";
- echo $row["state"] . " ";
- echo $row["country"] . " ";
- echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>";
- print("<br>");
- }
- } else {
- echo "0 results";
- }
- print("<br><br>");
+ print("Delete completed.");
  print("</html>");
  mysqli_close($conn);
-} else {
- print("No access.");
 }
 ?>
